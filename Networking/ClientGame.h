bd736aa09e00e56917688a40eee49d3f0c7b5637
April Hsu <azhsu2014@gmail.com>
2025-04-10 10:15:53 -0700

    initial client/server code

---
+++ Networking/ClientGame.h
@@ -0,0 +1,18 @@
+#include <winsock2.h>
+#include <Windows.h>
+#include "ClientNetwork.h"
+#include "NetworkData.h"
+
+class ClientGame
+{
+
+public:
+
+    ClientGame();
+    ~ClientGame(void);
+
+    ClientNetwork* network; 
+    void sendActionPackets();
+    char network_data[MAX_PACKET_SIZE];
+    void update();
+};
