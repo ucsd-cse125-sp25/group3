c051d1aa4385ec711afd6598aa46d2f079b59af1
angie-nguyen <ann087@sdsc.edu>
2025-04-28 11:28:34 -0700

    Unit tests for packets

--- packets/packet.cpp
+++ packets/packet.cpp
@@ -7,15 +7,21 @@ unsigned int Packet::getSize() {
 void Packet::serialize(char * data) {
     memcpy(data, &packet_type, sizeof(unsigned int));
     memcpy(&data[4], &length, sizeof(unsigned int));
-    // memcpy(&data[8], message, payload.size()); 
-    if (length > 0)
-    std::copy(payload.begin(), payload.end(), &data[8]);
+    if (getSize() > MAX_PACKET_SIZE) {
+        throw std::runtime_error("Packet size is greater than maximum size permitted");
+    }
+    else if (length > 0) {
+        std::copy(payload.begin(), payload.end(), &data[8]);
+    }
 }
 
 void Packet::deserialize(char * data) {
     memcpy(&packet_type, data, sizeof(unsigned int));
     memcpy(&length, &data[4], sizeof(unsigned int));
-    if (length > 0)
-    payload.insert(payload.begin(), &data[8], &data[8 + length]);
-    // memcpy(message, &data[8], sizeof message);  
+    if (length > MAX_PACKET_SIZE - 8) {
+        throw std::runtime_error("Packet size is greater than maximum size permitted");
+    }
+    else if (length > 0) {
+        payload.insert(payload.begin(), &data[8], &data[8 + length]);
+    }
 }
